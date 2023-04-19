f931bd20f67fd52e9733f02b1baed5c84a175288
Sohrab775 <sohrabsingh775@gmail.com>
2023-04-19 21:35:07 +0530

    first commit

---
+++ ArrayObject.js
@@ -0,0 +1,10 @@
+var a = {
+fname:"Sohrab",
+lname:"Sandhu",
+Age:26,
+email:"Sohrab@abc.com"
+};
+document.write(a+"<br><br>");
+document.write(a.email+"<br><br>");
+console.log(a);
+console.log(a.fname);
---
+++ IsArray.js
@@ -0,0 +1,10 @@
+var a=["Tom","Henderson",1,20,true,30.5];
+var b=89;
+if(Array.isArray(b))
+{
+  document.write("It's an array");
+
+}
+else {
+  document.write("It's not an array");
+}
